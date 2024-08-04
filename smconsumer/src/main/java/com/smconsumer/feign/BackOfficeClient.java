7cff8e7a1630a5d5d6e150255fba39af678f4449
matheuscolaresdasilva <matheuscolaresdasilva@gmail.com>
2024-08-03 19:48:29 -0600

    docker files e compose

--- smconsumer/src/main/java/com/smconsumer/feign/BackOfficeClient.java
+++ smconsumer/src/main/java/com/smconsumer/feign/BackOfficeClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "smbackoffice", url = "http://localhost:8082")
+@FeignClient(name = "smbackoffice", url = "${feign.client.config.smbackoffice.url}")
 public interface BackOfficeClient {
 
     @PostMapping("/api/v1/deduction")
